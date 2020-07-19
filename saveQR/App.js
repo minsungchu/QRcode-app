1d2ff26958ce4b70468cc93fb473f5ea5fa1ab18
mschu <chuminsung@gmail.com>
2020-07-20 07:27:41 +0900

    Make default saveQR

---
+++ saveQR/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  AppRegistry,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import {RNCamera} from "react-native-camera";
+
+
+// const App: () => React$Node = () => {
+//
+// }
+
+
+
+class App extends Component {
+  onSuccess(e) {
+    Linking
+        .openURL(e.data)
+        .catch(err => console.error('An error occured', err));
+  }
+
+  render() {
+    return (
+        <QRCodeScanner
+            onRead={this.onSuccess.bind(this)}
+            topContent={
+              <Text style={styles.centerText}>
+                Scan the QR code.
+              </Text>
+            }
+            bottomContent={
+              <TouchableOpacity style={styles.buttonTouchable}>
+                <Text style={styles.buttonText}>OK. Got it!</Text>
+              </TouchableOpacity>
+            }
+        />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
+
+export default App;
